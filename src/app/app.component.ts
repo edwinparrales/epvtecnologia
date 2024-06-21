eff712d29d8542a9f9f5b547c5c7fb64ebdc07e7
edwinparrales <edwinparrales82@gmail.com>
2024-06-21 02:57:38 -0500

    Adicionando estilos, paginas y modulos del sitio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import {SeoService} from "./services/seo.service";
 import {NavbarComponent} from "./components/navbar/navbar.component";
 import {FooterComponent} from "./components/footer/footer.component";
+import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,11 @@ import {FooterComponent} from "./components/footer/footer.component";
 export class AppComponent{
 
   title = 'epvsite';
-
+  constructor(private titulo:Title,private meta:Meta) {
+    meta.updateTag({name:"description",content:"Pagina personal Epvtecnologia sobre tecnologia desarrollo de software electronica"})
+    meta.updateTag({name:"keywords",content:"Tecnologia, desarrollo de software, electronica,computadores"})
+    this.title = 'epvsite';
+  }
 
 
 }
