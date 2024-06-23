ae9a9f9ad5d6251455f46bcfd0be5aa6d3ebb161
edwinparrales <edwinparrales82@gmail.com>
2024-06-23 01:16:18 -0500

    Desarrollo de la pagína de servicio

---
+++ src/app/components/btn-whatsapp/btn-whatsapp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-whatsapp',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn-whatsapp.component.html',
+  styleUrl: './btn-whatsapp.component.css'
+})
+export class BtnWhatsappComponent {
+
+}
