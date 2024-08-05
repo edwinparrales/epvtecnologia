610b7518793cf2fd4da45324b00681add87e3bee
edwinparrales <edwinparrales82@gmail.com>
2024-08-04 22:11:51 -0500

    adicion del modulo blog

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import {InicioComponent} from "./pages/inicio/inicio.component";
 import {AboutComponent} from "./pages/about/about.component";
 import {ServiciosComponent} from "./pages/servicios/servicios.component";
 import {GeneralesComponent} from "./pages/generales/generales.component";
+import { BlogComponent } from './components/blog/blog.component';
 
 export const routes: Routes = [
   {path:"inicio",component:InicioComponent},
   {path:"about",component:AboutComponent},
   {path:"servicios",component:ServiciosComponent},
   {path:"generales",component:GeneralesComponent},
+  {path:"blog",component:BlogComponent},
   {path:"**",redirectTo:"inicio",pathMatch:"full"}
 ];
